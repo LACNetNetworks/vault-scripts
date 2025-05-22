378fe141ca5e5b2b71b47e690c19d3d10efe42c7
amirandalac <amiranda@lacnet.com>
2025-05-22 18:59:56 +0200

    updated scripts

--- script/gas-model/memberLockToken.js
+++ script/gas-model/memberLockToken.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const { ethers, getAddress } = require("ethers");
 const { getSigner } = require("../utils/provider");
 const contractAbi = require("../../artifacts/src/gas-model/VaultIn.sol/VaultIn.json");
+const erc20Abi = require("../../artifacts/src/gas-model/ERC20Mock.sol/ERC20Mock.json");
 
 async function main() {
-  const signer = getSigner(process.env.PRIVATE_KEY_OWNER);
+  const signer = getSigner(process.env.PRIVATE_KEY_MEMBER);
 
   deployedContract = new ethers.Contract(
     process.env.VAULT_CONTRACT,
